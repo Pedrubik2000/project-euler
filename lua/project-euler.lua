dccb595bf5dd419dc3f5283e318240f4f2d61018
Pedro Echevarria <pedroechevarria.us@gmail.com>
2025-01-02 07:52:59 -0500

    try wrap function

--- lua/project-euler.lua
+++ lua/project-euler.lua
@@ -2,7 +2,7 @@ local function multiplesof3and5()
    vim.notify(require("multiplesof3and5"))
 end
 local function setup()
-    vim.api.nvim_create_user_command('Multiplesof3and5',multiplesof3and5(),{})
+    vim.api.nvim_create_user_command('Multiplesof3and5',function () multiplesof3and5() end,{})
 end
 
 return { setup = setup }
