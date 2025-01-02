a2a135c809851423c8f0c6f2a3e8b89e328bc6c1
Pedro Echevarria <pedroechevarria.us@gmail.com>
2025-01-02 07:50:51 -0500

    fixed uppercase typo

--- lua/project-euler.lua
+++ lua/project-euler.lua
@@ -2,7 +2,7 @@ local function multiplesof3and5()
    vim.notify(require("multiplesof3and5"))
 end
 local function setup()
-    vim.api.nvim_create_user_command('multiplesof3and5',multiplesof3and5(),{})
+    vim.api.nvim_create_user_command('Multiplesof3and5',multiplesof3and5(),{})
 end
 
 return { setup = setup }
